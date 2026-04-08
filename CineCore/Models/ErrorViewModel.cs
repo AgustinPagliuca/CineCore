93cb24c849f124ea692fc51e8395c5dc8c0da40a
Agustin Pagliuca <agustinpagliuca1@gmail.com>
2026-04-08 14:40:31 -0300

    Setup inicial del proyecto

---
+++ CineCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
